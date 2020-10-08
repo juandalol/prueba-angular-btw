e345599f3d51e7cd559115c3e730ed5ed5026aaf
Juan David Gómez <juandagomez3@gmail.com>
2020-10-07 20:06:46 -0500

    creación de los componentes para el registro y login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
+  { path: 'login', loadChildren: () => import('./components/auth/login/login.module').then(m => m.LoginModule) },
+  { path: 'register', loadChildren: () => import('./components/auth/register/register.module').then(m => m.RegisterModule) }
   // {path: '', component:}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material/material.module';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
